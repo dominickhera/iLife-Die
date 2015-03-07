e6790e07fb2382212d38ae1532ef9edba34750b8
Dominick Hera <dominickhera@Dominicks-MacBook-Pro.local>
2015-03-06 20:06:05 -0500

    Initial Commit

---
+++ iLife Die/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iLife Die
+//
+//  Created by Dominick Hera on 3/6/15.
+//  Copyright (c) 2015 Posa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
